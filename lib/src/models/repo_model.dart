7caa781f3aaf840e8e74d3f498dca63ad19b8898
Musa Musa <musabrillz@gmail.com>
2020-04-07 17:57:13 +0100

    model created for repositories

--- lib/src/models/repo_model.dart
+++ lib/src/models/repo_model.dart
@@ -1,25 +1,41 @@
 class Repo {
-   String totalCount;
+   int totalCount;
    List<Item> items;
 
    Repo({this.totalCount, this.items});
 
    Repo.fromJson(Map<String, dynamic> json) {
+      totalCount = json["total_count"];
+      items = new List<Item>();
 
+      json["items"].forEach((v) {
+        items.add(new Item.fromJson(v));
+      });
    }
 }
 
 class Item {
-  final String name;
-  final Owner owner;
+  String name;
+  Owner owner;
 
   Item({this.name, this.owner});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    name = json["name"];
+    owner = new Owner();
+  }
 }
 
 class Owner {
-  final String login;
-  final String avatarUrl;
-  final String type;
+  String login;
+  String avatarUrl;
+  String type;
 
   Owner({this.login, this.avatarUrl, this.type});
+
+  Owner.fromJson(Map<String, dynamic> json) {
+    login = json["login"];
+    avatarUrl = json["avarter_url"];
+    type = json["type"];
+  }
 }
